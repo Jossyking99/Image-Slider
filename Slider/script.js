f871b8c7f2820726dba9c9680d87a6605b1bfee1
Joe-Web <95709035+Jossyking99@users.noreply.github.com>
2022-11-01 18:51:07 +0100

    Added some files

---
+++ Slider/script.js
@@ -0,0 +1,18 @@
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+const container = document.querySelector('.images');
+
+let counter = 0;
+
+nextBtn.addEventListener('click',nextSlide);
+prevBtn.addEventListener('click',prevSlide);
+
+function nextSlide(){
+    if(counter === 4){
+        counter = -1;
+    }
+
+    counter++;
+
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpg`
+}
